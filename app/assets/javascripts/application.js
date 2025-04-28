214d496a23d87bf5bed28e0321123aab0a5f5e0e
AlexMoraCardona <proyectostecnologicosweb.outlook.com>
2025-04-28 18:21:50 -0500

    Correccion de graficos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,25 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "popper"
-import "bootstrap" 
-import "chartkick"
-import "Chart.bundle"
-
 //= require chartkick
 //= require Chart.bundle
-
-//import "direct_uploads"
-require("trix")
-require("@rails/actiontext")
-
-setTimeout((function () {
-    window.status = "FLAG_FOR_PDF";
-    Object.keys(Chartkick.charts).forEach(function (key) {
-        Chartkick.charts[key].redraw();
-    });
-}), 3000);
-
-
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+//= require("trix")
+//= require("@rails/actiontext")
+//= require_tree .
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,26 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
 import "popper"
 import "bootstrap" 
-
 import "chartkick"
 import "Chart.bundle"
 import "chartkick/chart.js"
-
-//import "direct_uploads"
+import "direct_uploads"
 import "trix"
 import "@rails/actiontext"
-
-setTimeout((function () {
-    window.status = "FLAG_FOR_PDF";
-    Object.keys(Chartkick.charts).forEach(function (key) {
-        Chartkick.charts[key].redraw();
-    });
-}), 3000);
-
-
-
-
-
